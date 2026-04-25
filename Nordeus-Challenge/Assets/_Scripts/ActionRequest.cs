84f8c0ea0b61c38e9184d8f18671513a43089823
urosh2003 <radukic.uros@gmail.com>
2026-04-25 22:30:53 +0200

    Add: Basic combat

---
+++ Nordeus-Challenge/Assets/_Scripts/ActionRequest.cs
@@ -0,0 +1,11 @@
+﻿
+[System.Serializable]
+public class ActionRequest
+{
+        public string moveId;
+
+        public ActionRequest(string actionId)
+        {
+                this.moveId = actionId;
+        }
+}
